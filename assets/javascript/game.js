1f0c89614cb76a60e199b556bec6144d41752b41
beardykraken <vcasmirri@gmail.com>
2020-01-20 21:37:26 -0500

    Added background, javascript functionality for game, edited HTML for crystals to populate.

---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+// Starting values
+var winCount = 0;
+var lossCount = 0;
+$("#wins").text("Wins: " + winCount);
+$("#losses").text("Losses: " + lossCount);
+
+// Stores image sources for crystals
+var crystalArray = ["assets/images/purple_crystal.jpg", "assets/images/blue_crystal.jpg", "assets/images/green_crystal.jpg", "assets/images/pink_crystal.jpg"]
+
+// Generates crystals on page and assigns random values to each.
+
+function crystalValues () {
+
+for (var i = 0; i < crystalArray.length; i++) {
+    var crystalImage = $("<img>");
+    crystalImage.addClass("crystal");
+    crystalImage.attr("src", crystalArray[i]);
+    crystalImage.attr("data-crystalvalue", Math.floor((Math.random() * 12) +1));
+    $("#crystals").append(crystalImage);
+}
+}
+
+// Sets up game
+
+function play() {
+
+var currentValue = 0;
+$("#current").text("My score: " + currentValue);
+
+var goalValue = Math.floor((Math.random() * (120-19) + 19));
+$("#goal").text("Goal : " + goalValue);
+console.log ("The current goal is " + goalValue);
+
+
+$(".crystal").on("click", function() {
+    
+    // Gets randomly-assigned value from crystal that user clicks and adds it to current score
+    
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    console.log("Current value of this crystal is " + crystalValue);
+    currentValue += crystalValue;
+    $("#current").text("My score: " + currentValue);
+    console.log("Current score is " + currentValue);
+
+    // Sets win condition, increments winCount, resets game
+
+    if (currentValue === goalValue) {
+        winCount++;
+        $("#wins").text("Wins: " + winCount);
+        $("#crystals").empty();
+        crystalValues();
+        play();
+    }
+
+    // Sets loss condition, increments lossCount, resets game
+
+    else if (currentValue >= goalValue) {
+        lossCount++;
+        $("#losses").text("Losses: " + lossCount);
+        $("#crystals").empty();
+        crystalValues();
+        play();
+    }
+
+});
+
+}
+
+// Calls functions for initial game set-up
+
+crystalValues();
+play();
+
+})
